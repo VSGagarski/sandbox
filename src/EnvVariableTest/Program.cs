42703aa183502ee5e6879a6863a45a67b1550762
vasyagaga <vasyagaga@mail.ru>
2020-07-12 14:09:52 +0300

    add Console App for test Enviroment Variables commit

---
+++ src/EnvVariableTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EnvVariableTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
